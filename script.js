7905f102e3ff4729caa40f94a887d73312f3052b
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-01 14:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+let table = document.querySelector("table")
+let price = document.querySelectorAll(".price")
+
+	let totalPriceSum = 0;
+
+	for(let p of price){
+		totalPriceSum += +p.innerText;
+	}
+
+
+	let tr = document.createElement("tr");
+	tr.id="ans";
+	let td1 = document.createElement("td");
+	let td2 = document.createElement("td");
+	
+    td1.innerText = "total price";
+	td2.innerText = totalPriceSum;
+	tr.append(td1,td2);
+
+	table.append(tr)
+	
+
+
+
+
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
